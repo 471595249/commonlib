df13a5ec86df279a5befb242894d1e8da6c644d1
xiezh <471595249@qq.com>
2019-10-18 10:08:53 +0800

    发布测试一个组件;

---
+++ src/components/BaseComponent.js
@@ -0,0 +1,34 @@
+/**
+ * Created by xzh on 20:19 2019-08-01
+ *
+ * @Description: 所有page页面继承此组件, 用于做全页面公用函数等功能;
+ */
+
+import React, {Component} from 'react';
+import {DeviceEventEmitter} from 'react-native';
+
+export default class BaseComponent extends Component {
+
+  // navigation 推出的所有视图, 默认去掉自带的顶部导航条
+  static navigationOptions = {
+    header: null
+  };
+
+  constructor(props) {
+    super(props);
+  }
+
+
+  sendNotification() {
+
+  }
+
+  Super_TokenExprie() {
+
+  }
+
+  recieveNotification() {
+
+  }
+
+}
